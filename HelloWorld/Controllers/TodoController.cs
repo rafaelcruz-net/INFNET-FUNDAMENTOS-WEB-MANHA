48895642bb44a1a173c9bd459f2fd1c1a2a9eed9
Rafael Cruz <rafaelcruz.net81@gmail.com>
2020-05-19 09:22:55 -0300

    Consolidando conceitos de Rotas, Controllers e View

---
+++ HelloWorld/Controllers/TodoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Todo> todos = new List<Todo>();
+
+            todos.Add(new Todo()
+            {
+                Nome = "Ver Email Trabalho",
+                Concluido = true
+            });
+
+            todos.Add(new Todo()
+            {
+                Nome = "Marcar dentista",
+                Concluido = false
+            });
+
+            todos.Add(new Todo()
+            {
+                Nome = "Fazer trabalho TP 3",
+                Concluido = false
+            });
+
+            todos.Add(new Todo()
+            {
+                Nome = "Limpar a louça",
+                Concluido = true
+            });
+
+
+            return View(todos);
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
---
+++ HelloWorld/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class Todo
+    {
+        public String Nome { get; set; }
+        public bool Concluido { get; set; }
+    }
+}
